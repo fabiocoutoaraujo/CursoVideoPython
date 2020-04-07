42623d7a40e123359cb52fc6bd7f044fe459f3a0
Fábio Couto Araujo <fabiocoutoaraujo@gmail.com>
2020-04-07 14:16:57 -0300

    Add celsius to fahreneit exercise

---
+++ mundo-01/aula07-ex014.py
@@ -0,0 +1,4 @@
+celsius = float(input("Informe a temperatura em ºC: ").strip())
+fahrenheit = celsius * 9 / 5 + 32
+print("A temperatura de {0} ºC corresponde a {1} ºF!" .format(celsius, fahrenheit))
+ 
